900144120a46b8249a07c5d9a6af205cc76e980b
samuelscocho <samuelscocho@gmail.com>
2026-09-24 23:49:44 +0200

    e4u5u

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let numberguess = Math.round(Math.random() * 100);
  let versuche = 0;
+ 
 function guess() {
  alert("es funktioniert!");
 
  versuche = versuche + 1; 
 
  displayversuche.innerHTML = 'Versuche: '+ versuche;
- 
+
 }
